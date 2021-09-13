d600df151a8b406c154d6a2b7083ad24b31a31de
Ahmad-Khaled-Zaid <ahmadk.zaid97@gmail.com>
2021-09-13 22:45:17 +0300

    upload backend2

--- controllers/books.controller.js
+++ controllers/books.controller.js
@@ -1,16 +1,29 @@
 'use strict';
+const { request, response } = require('express');
 const bookModel = require('../models/Book.model')
 // const { response, request } = require('express')
 
 const getbooks = (request, response) => {
-    bookModel.find(( error,booksData) => {
-        if(error){
+    bookModel.find((error, booksData) => {
+        if (error) {
             response.send(error)
         }
         response.json(booksData)
     })
 };
+const createBook = (request, response) => {
+
+    const { title, description, status, email } = request.body;
+    const newBook=new booksModel({
+        title, description, status, email 
+    });
+    newBook.save();
+    response.json(newBook)
+}
+   
 
 module.exports = {
-    getbooks
+    getbooks,
+    createBook
 }
+//  console.log(request.body);
