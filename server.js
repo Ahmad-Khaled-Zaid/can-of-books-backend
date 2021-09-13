266ac6842023f0cd72164fd2a5919a6f3b77f1e1
Ahmad-Khaled-Zaid <ahmadk.zaid97@gmail.com>
2021-09-13 11:16:31 +0300

    fix some issue

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 'use strict';
+require('dotenv').config();
 
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
-require('dotenv').config();
 app.use(cors());
 
 
 const PORT = process.env.PORT || 4321;
-const MONGO_URL = process.env.MONGO_URL;
+// const MONGO_URL = process.env.MONGO_URL;
 const DB_NAME = process.env.DB_NAME;
-const url = 'mongodb://127.0.0.1:27017/game-of-thrones'
+const url = 'mongodb://127.0.0.1:27017/'
 
 mongoose.connect(`${url}/${DB_NAME}`);
-const seeed= require('./helper/bookSeeds.seeds')
-seeed();
+
 const getIndex = require('./controllers/index.controller')
 app.get('/', getIndex)
 const {getbooks}=require('./controllers/books.controller')
 app.get('/booksCollection',getbooks)
 // app.get('/getbooks',getbooks)
 
-
+const seeed= require('./helper/bookSeeds.seeds')
+seeed();    
 
 app.listen(PORT, () =>{
     console.log(`Server Started on port ${PORT}`);
